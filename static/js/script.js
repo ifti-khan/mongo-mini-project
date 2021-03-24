8439b056af56616622195b189c424b3e730e7d7b
ifti-khan <ifti_k@hotmail.com>
2021-03-24 14:43:43 +0000

    bound data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,6 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-    $(".collapsible").collapsible();    
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();    
 });
